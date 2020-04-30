edbf1b1486423ca3161e34357b4170422d52dc1f
Kang Gyeong Seok <rkdrudtjr159@gmail.com>
2020-04-30 19:58:12 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/side/tmaxmap/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.side.tmaxmap
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
