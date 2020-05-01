ef49f207f6a78104cf44ccf0484b3ae568b8ccb0
Kang Gyeong Seok <rkdrudtjr159@gmail.com>
2020-05-01 16:05:17 +0900

    [Tutorial] #2 ListView

--- lib/main.dart
+++ lib/main.dart
@@ -22,33 +22,32 @@ class MyHomePage extends StatelessWidget {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("side project for tmax"),
+        title: Text("tutorial for flutter"),
     ),
-      body: Column(
-        children: <Widget>[
-          Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0)
-          ),
-          Container(
-              color: Colors.green,
-              width: 100,
-              height: 100,
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0)
-          ),
-          Container(
-              color: Colors.blue,
-              width: 100,
-              height: 100,
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0)
-          ),
-        ],
-      )
+      body:
+        ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              trailing: Icon(Icons.navigate_next),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.event),
+              title : Text("Event"),
+              trailing: Icon(Icons.navigate_next),
+              onLongPress: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.camera),
+              title: Text('Camera'),
+              trailing: Icon(Icons.navigate_next),
+              onTap: (){},
+            )
+          ],
+        )
 
 
     );
