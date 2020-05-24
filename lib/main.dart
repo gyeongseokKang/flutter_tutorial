ac3108d299f99f9ee481120358b1297bf245545c
Kangkyeungseok <kyeungseok.kang@gmail.com>
2020-05-24 14:34:03 +0900

    [Tutorial] #13 checkboxTileList

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,50 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(MyApp());
 
+/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
+  static const String _title = 'CheckboxListTile Example';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FirstPage(),
-      routes: {
-        '/first' : (context) => FirstPage(),
-        '/second' : (context) => SecondPage(),
-      },
-    );
-  }
-}
-class FirstPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("첫 페이지"),
-      ),
-      body: Center(
-        //fea
-        child: RaisedButton(
-          child: Text('다음 Page'),
-          onPressed: () async{
-            final result = await Navigator.pushNamed(context, '/second');
-            print(result);
-          },
-
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: Center(
+          child: MyStatefulWidget(),
         ),
-      )
-      ,
+      ),
     );
   }
 }
 
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
 
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
 
-class SecondPage extends StatelessWidget{
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  bool _ischecked = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("2번 페이지"),
-      ),
-      body: Center(
-          child : RaisedButton(
-            child: Text(' 이전 페이지 '),
-            onPressed: (){
-              Navigator.pop(context,'데아터 돌려줌');
-            },
-          ),
-      ),
+    return CheckboxListTile(
+      title: const Text('title : CheckBox'),
+      subtitle: const Text('subtitle : click me!'),
+      value: _ischecked,
+      onChanged: (bool value) {
+        setState(() {
+          _ischecked = value;
+
+        });
+      },
+      secondary: const Icon(Icons.home),
+      activeColor: Colors.red,
+      checkColor: Colors.black,
+      isThreeLine: false,
+      selected: _ischecked,
     );
   }
 }
